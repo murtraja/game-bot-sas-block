150c656408aa233f4971c2848859b89150505bd7
Murtaza Raja <murtraja@gmail.com>
2019-10-23 21:25:19 +0530

    working

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import time
 
-import screen_grab
+import screen_grab, process
 
 def get_screen():
 	img = screen_grab.grab_screen()
@@ -13,9 +14,16 @@ def show_screen(img):
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
 
-def main():
+def save_screen():
 	img = get_screen()
 	cv2.imwrite('s.png', img)
+
+def main():
+	while True:
+			img = get_screen()
+			process.process(img)
+			time.sleep(0.2)
+
 
 if __name__ == '__main__':
 	main()
--- process.py
+++ process.py
@@ -5,54 +5,40 @@ from pymouse import PyMouse
 import screen_grab
 
 char_radius = 75
+m = PyMouse()
 
 def imshowbgr(name, img):
     hsv_img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
     cv2.imshow(name, hsv_img)
 
-def main():
-    img = cv2.imread('s.png')
-    # cv2.imshow('s', img)
-
-    # c1 = round(img.shape[0]/2)
-    # c2 = round(img.shape[1]/2)
-    # img_center = img[c1-char_radius:c1+char_radius, c2-char_radius:c2+char_radius, ...]
-    # cv2.imshow('c', img_center)
-
+def process(img):
     blurred = cv2.GaussianBlur(img, (11, 11), 0)
-    # cv2.imshow('d', blurred)
 
     lower = (175, 50, 50)
     higher = (180, 255, 255)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-    # imshowbgr('hsv', hsv)
-
 
     # # Find a mask using the lower and higher bounds
     mask = cv2.inRange(hsv, lower, higher)
-    # for d1 in mask:
-    #     for d2 in d1:
-    #         if d2 > 0:
-    #             print("found", d2)
-    # cv2.imshow('inRange', mask)
     mask = cv2.erode(mask, None, iterations=2)
-    # cv2.imshow('erode', mask)
     mask = cv2.dilate(mask, None, iterations=2)
-    # cv2.imshow('dilate', mask)
 
     # # Find contours
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     # print(len(cnts))
+    if len(cnts) <= 0:
+        return
+
     ((x,y), rad) = cv2.minEnclosingCircle(cnts[0])
     x = round(x)
     y = round(y)
     s_x = screen_grab.top_left[0] + x
     s_y = screen_grab.top_left[1] + y
-    m = PyMouse()
     m.move(s_x, s_y)
-    # print (x,y,rad)
-    # circled = cv2.circle(mask, (round(x), round(y)), round(rad)+20, 255)
-    # cv2.imshow('cir', circled)
+
+def main():
+    img = cv2.imread('s.png')
+    process(img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
